ff2849d9655265e3bf994d2daeec381d22c39e60
pco <ddd@gmai.com>
2017-08-17 00:38:40 +0900

    changes sysout

--- BookStore/src/main/java/com/bookstore/bookpack/HomeController.java
+++ BookStore/src/main/java/com/bookstore/bookpack/HomeController.java
@@ -45,8 +45,9 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("ddd");
 		return "test";
 	}	
+	
 	
 }
